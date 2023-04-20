f26b87ac73a41be9afca80cdebeb1e711888c7b1
Fabio <fabiomt.98@hotmail.com>
2023-04-19 21:24:39 -0300

    corrigida a palavra quanto

--- Aulas/metodos-de-strings/11-trim.js
+++ Aulas/metodos-de-strings/11-trim.js
@@ -1,4 +1,4 @@
-//remove os espaços em branco tanto no inicio quanotno fim de uma string
+//remove os espaços em branco tanto no inicio quanto no fim de uma string
 
 const email = '        jose@cubos.academy   ';
 const formatado = email.trim();
