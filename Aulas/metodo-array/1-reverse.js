a12d6f43e37a097d35400329b43362005b098559
Fabio <fabiomt.98@hotmail.com>
2023-04-24 17:05:54 -0300

    Finalizado as aulas de metodos de array 1

---
+++ Aulas/metodo-array/1-reverse.js
@@ -0,0 +1,7 @@
+// Para inverter as propriedades do array
+
+const opcoes = ['eu', 'voce', 'ele'];
+
+const resultado = opcoes.reverse(); // modifica o original
+console.log(resultado);
+console.log(opcoes);
---
+++ Aulas/metodo-array/9-valor-e-referencia.js
@@ -0,0 +1,18 @@
+// let numero1 = 5;
+// let numero2 = numero1;
+// numero1 = 10;
+// console.log(numero1);
+// console.log(numero2);
+
+// let texto1 = 'abra';
+// let texto2 = texto1;
+// texto1 = texto1 + 'cadabra';
+// console.log(texto1);
+// console.log(texto2);
+
+//objetos e array se tornam um so
+let obj1 = { prop: 5};
+let obj2 = obj1;
+obj1.prop = 10;
+console.log(obj1);
+console.log(obj2);
