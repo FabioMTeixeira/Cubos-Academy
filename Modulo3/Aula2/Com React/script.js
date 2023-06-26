bf393ad4e2aab74892b054426f1a0bd5b5ccfb4e
Fabio <fabiomt.98@hotmail.com>
2023-06-26 17:06:04 -0300

    example

---
+++ Modulo3/Aula2/Com React/script.js
@@ -0,0 +1,23 @@
+class HelloWorld extends React.Component {
+
+  render() {
+    return (
+      React.createElement(
+        'div',
+        {},
+        React.createElement(
+          'h1',
+          {},
+          'Hello World',
+        ),
+        React.createElement(
+          'h2',
+          {},
+          Math.random(),
+        ),
+      )
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(HelloWorld)), document.querySelector('#root');
