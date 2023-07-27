c7dd321b133f39cdecaa3771db832b733ab2924b
Fabio <fabiomt.98@hotmail.com>
2023-07-27 10:11:38 -0300

    feat: validations

--- Modulo4/Formulário React/Exemplo1/src/pages/Main/index.jsx
+++ Modulo4/Formulário React/Exemplo1/src/pages/Main/index.jsx
@@ -4,27 +4,61 @@ import './style.css';
 function Main() {
   const [name, setName] = useState('');
   const [age, setAge] = useState(0);
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log('teste');
+    setError('');
+    setSuccess('');
+
+    if (!name) {
+      setError('Nome Obrigatório...');
+      return;
+    }
+
+    if (age < 18) {
+      setError('Deve ser maior de idade');
+      return;
+    }
+
+    if (password.length < 8) {
+      setError('Senha precisa ter 8 caracter');
+      return;
+    }
+
+    setSuccess('Cadastro efetuado com sucesso');
   }
 
   return (
     <div className='container-main'>
       <form onSubmit={handleSubmit}
       >
-        <input type="text"
+        <input
+          type="text"
           placeholder='Digite seu nome'
           value={name}
           onChange={(event) => setName(event.target.value)}
         />
-        <input type="number"
+        <input
+          type="number"
           placeholder='Digite sua idade'
           value={age}
           onChange={(event) => setAge(event.target.valueAsNumber)}
         />
+        <input
+          type="password"
+          placeholder='Digite a sua senha'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <span className='error'>{error}</span>}
+
+        {success && <span className='success'>{success}</span>}
+
         <button type='submit'>Salvar</button>
       </form>
     </div>
