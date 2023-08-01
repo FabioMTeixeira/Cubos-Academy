248ab8a870c8a713d4ed3131e4b64eef8a480c1b
Fabio <fabiomt.98@hotmail.com>
2023-08-01 20:19:50 -0300

    chore: change name and looping

--- Modulo4/Back/Primeiro Express/src/index.js
+++ Modulo4/Back/Primeiro Express/src/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const nomes = ['bruno', 'bruna', 'fernando', 'fernanda'];
+let i = 0;
+
 app.get('/', (req, res) => {
-    res.send('Olaa primeiro servidor');
+    if (i < nomes.length) {
+        res.send(`Olaa primeiro servidor ${nomes[0 + i]}`);
+        console.log(nomes.length);
+        console.log(i);
+        i++;
+    } else {
+        i = 0;
+        res.send('Acabou');
+    }
 });
 
 app.listen(3000);
