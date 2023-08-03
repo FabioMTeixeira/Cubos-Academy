66d5da0fc1ccd5b9d84cfc46ffe23cf4025c337c
Fabio <fabiomt.98@hotmail.com>
2023-08-03 15:33:38 -0300

    create new router

--- Modulo4/Back/Rotas-intermediarios-controles/src/index.js
+++ Modulo4/Back/Rotas-intermediarios-controles/src/index.js
@@ -2,9 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send('bora');
+const professores = [
+    {
+        id: 1,
+        nome: 'fabio',
+        stack: 'backend'
+    },
+    {
+        id: 2,
+        nome: 'dani',
+        stack: 'frontend'
+    },
+    {
+        id: 3,
+        nome: 'guido',
+        stack: 'backend'
+    }
+];
+
+app.get('/professores', (req, res) => {
+    res.send(professores);
+});
+
+app.get('/professores/:id', (req, res) => {
+    const { id } = req.params;
+
+    const professorEncontrado = professores.find((professor) => {
+        return professor.id === Number(id);
+    });
+
+    res.send(professorEncontrado);
 });
 
 app.listen(3000, () => {
